05c274a0f2934873d9c2186a23044ff7814d03b6
shubhampatel <shubham.patel@sliceit.com>
2020-11-08 01:15:19 +0530

    feat:- scan criteria screen viewmodel & repo created with di

---
+++ app/src/main/java/com/shubham/marketpulse/features/home/scancriteria/repository/ScanCriteriaRepository.kt
@@ -0,0 +1,10 @@
+package com.shubham.marketpulse.features.home.scancriteria.repository
+
+import com.shubham.marketpulse.data.DataHelper
+import javax.inject.Inject
+
+/**
+ * Created by shubhampatel on 08 Nov, 2020.
+ */
+class ScanCriteriaRepository @Inject constructor(private val dataHelper: DataHelper) {
+}
--- app/src/main/java/com/shubham/marketpulse/features/home/scancriteria/viewmodel/ScanCriteriaFragmentViewModel.kt
+++ app/src/main/java/com/shubham/marketpulse/features/home/scancriteria/viewmodel/ScanCriteriaFragmentViewModel.kt
@@ -1,10 +1,13 @@
 package com.shubham.marketpulse.features.home.scancriteria.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.shubham.marketpulse.features.home.scancriteria.repository.ScanCriteriaRepository
 import javax.inject.Inject
 
 /**
  * Created by shubhampatel on 08 Nov, 2020.
  */
-class ScanCriteriaFragmentViewModel @Inject constructor(): ViewModel() {
+class ScanCriteriaFragmentViewModel @Inject constructor(
+    private val repository: ScanCriteriaRepository
+) : ViewModel() {
 }
