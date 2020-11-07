d646c1a3b039b26186758ebb243cb8f77fc1892a
shubhampatel <shubham.patel@sliceit.com>
2020-11-08 01:15:06 +0530

    feat:- scans list screen viewmodel & repo created with di

---
+++ app/src/main/java/com/shubham/marketpulse/features/home/scanslisting/repository/ScansListingRepository.kt
@@ -0,0 +1,10 @@
+package com.shubham.marketpulse.features.home.scanslisting.repository
+
+import com.shubham.marketpulse.data.DataHelper
+import javax.inject.Inject
+
+/**
+ * Created by shubhampatel on 08 Nov, 2020.
+ */
+class ScansListingRepository @Inject constructor(private val dataHelper: DataHelper) {
+}
--- app/src/main/java/com/shubham/marketpulse/features/home/scanslisting/viewmodel/ScansListingFragmentViewModel.kt
+++ app/src/main/java/com/shubham/marketpulse/features/home/scanslisting/viewmodel/ScansListingFragmentViewModel.kt
@@ -1,10 +1,13 @@
 package com.shubham.marketpulse.features.home.scanslisting.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.shubham.marketpulse.features.home.scanslisting.repository.ScansListingRepository
 import javax.inject.Inject
 
 /**
  * Created by shubhampatel on 08 Nov, 2020.
  */
-class ScansListingFragmentViewModel @Inject constructor(): ViewModel() {
+class ScansListingFragmentViewModel @Inject constructor(
+    private val repository: ScansListingRepository
+) : ViewModel() {
 }
