f0fd97cdce564eb73242e1d45a8dd659dd59bb29
shubhampatel <shubham.patel@sliceit.com>
2020-11-07 21:22:36 +0530

    feat:- fragments classes/layouts created

---
+++ app/src/main/java/com/shubham/marketpulse/features/home/scancriteria/view/ScanCriteriaFragment.kt
@@ -0,0 +1,10 @@
+package com.shubham.marketpulse.features.home.scancriteria.view
+
+import com.shubham.marketpulse.R
+import com.shubham.marketpulse.base.BaseFragment
+
+/**
+ * Created by shubhampatel on 07 Nov, 2020.
+ */
+class ScanCriteriaFragment: BaseFragment(R.layout.fragment_scan_criteria) {
+}
---
+++ app/src/main/java/com/shubham/marketpulse/features/home/scanslisting/view/ScansListingFragment.kt
@@ -0,0 +1,10 @@
+package com.shubham.marketpulse.features.home.scanslisting.view
+
+import com.shubham.marketpulse.R
+import com.shubham.marketpulse.base.BaseFragment
+
+/**
+ * Created by shubhampatel on 07 Nov, 2020.
+ */
+class ScansListingFragment: BaseFragment(R.layout.fragment_scans_listing) {
+}
