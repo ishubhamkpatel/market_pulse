a78f4a334f8c7caaa6bdd54cb3692b2ff3595501
shubhampatel <shubham.patel@sliceit.com>
2020-11-09 16:40:23 +0530

    feat:- splash functionality implemented

--- app/src/main/java/com/shubham/marketpulse/features/splash/SplashActivity.kt
+++ app/src/main/java/com/shubham/marketpulse/features/splash/SplashActivity.kt
@@ -1,7 +1,37 @@
 package com.shubham.marketpulse.features.splash
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.shubham.marketpulse.R
 import com.shubham.marketpulse.base.BaseActivity
+import com.shubham.marketpulse.features.home.HomeActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : BaseActivity(R.layout.activity_splash) {
+
+    companion object {
+        private const val WAITING_DURATION = 5000L // 5 seconds
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        waitAndLaunchHome()
+    }
+
+    private fun waitAndLaunchHome() {
+        lifecycleScope.launch {
+            delay(WAITING_DURATION)
+            launchHome()
+        }
+    }
+
+    private fun launchHome() {
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        startActivity(intent)
+        finish()
+    }
 }
